701bbc9742a497cbb734777b1c7d7278b9c1f4a2
hewking <604910070qq.com>
2016-12-01 23:16:10 +0800

    complete symmertictree

--- app/src/main/java/leetcode/SymmetricTree.java
+++ app/src/main/java/leetcode/SymmetricTree.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * Created by hewking on 2016/11/30.
  * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
@@ -20,4 +22,84 @@ package leetcode;
  */
 
 public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+
+        if(root == null){
+            return true;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        if(root.left != null){
+            if(root.right == null){
+                return false;
+            }
+            stack.push(root.left);
+            stack.push(root.right);
+        }else{
+            if(root.right != null){
+                return false;
+            }
+        }
+
+        while(!stack.empty()){
+            if(stack.size()%2 != 0) return false;
+            TreeNode left = stack.pop();
+            TreeNode right = stack.pop();
+            if(left.val != right.val){
+                return false;
+            }
+            if(left.left != null){
+                if(right.right == null){
+                    return false;
+                }
+                stack.push(left.left);
+                stack.push(right.right);
+            }else{
+                if(right.right != null){
+                    return false;
+                }
+            }
+
+            if(left.right != null){
+                if(right.left == null){
+                    return false;
+                }
+                stack.push(left.right);
+                stack.push(right.left);
+            }else{
+                if(right.left != null){
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    public boolean isSymmetric(SymmetricTreeNode root) {
+        if(root != null){
+            return isSymmetricPlus(root.left,root.right);
+        }
+        return false;
+    }
+
+    private boolean isSymmetricPlus(SymmetricTreeNode left, SymmetricTreeNode right) {
+        if(left == null || right == null){
+            return left == right;
+        }
+        if(left.val != right.val){
+            return false;
+        }
+        return isSymmetricPlus(left.left,right.right)  && isSymmetricPlus(left.right,right.left);
+    }
 }
+
+ class SymmetricTreeNode {
+    int val;
+    SymmetricTreeNode left;
+    SymmetricTreeNode right;
+    SymmetricTreeNode(int x) { val = x; }
+}
+
+
