0e3a4224f35e80a9e106dd71a92985bb8e464cf9
hewking <604910070qq.com>
2019-04-19 01:23:09 +0800

    add RemoveLinkedListElement and RemoveDuplicatesFromSortedArray

--- leetcode2/src/leetcode/RemoveLinkedListElements.java
+++ leetcode2/src/leetcode/RemoveLinkedListElements.java
@@ -12,7 +12,17 @@ package leetcode;
 public class RemoveLinkedListElements {
 
     public ListNode removeElements(ListNode head, int val) {
-
+        ListNode header = new ListNode(-1);
+        header.next = head;
+        ListNode cur = header;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return header.next;
     }
 
 
