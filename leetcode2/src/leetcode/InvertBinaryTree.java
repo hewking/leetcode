f875003280a0e48e0aa6a499653f98fcb1caebf0
hewking <604910070@qq.com>
2019-04-30 18:13:18 +0800

    add 翻转二叉树  二叉树最小深度

---
+++ leetcode2/src/leetcode/InvertBinaryTree.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/invert-binary-tree/
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ *
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ * @program: leetcode
+ * @description: ${description}
+ * @author: hewking
+ * @create: 2019-04-30 17:48
+ * 这个题牛逼了，大佬面试google 都搞不定 要是我搞定了 哈哈 服一下自己
+ **/
+public class InvertBinaryTree {
+
+    /**
+     * 这个题一看就跟 对称二叉树差不多。
+     * 思路：
+     * 1.重点在于递归
+     * 2. 在于对每个字数的翻转？
+     * 怎么翻转 tmp = root.left;
+     * root.left = root.right;
+     * root.right = tmp
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+}
---
+++ leetcode2/src/leetcode/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+/**
+ * 二叉树得最小深度
+ * https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
+ * 给定一个二叉树，找出其最小深度。
+ *
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最小深度  2.
+ * @program: leetcode
+ * @description: ${description}
+ * @author: hewking
+ * @create: 2019-04-30 17:30
+ **/
+public class MinimumDepthOfBinaryTree {
+
+    /**
+     * 注意有一个坑得地方 [1,2]。
+     * 最小深度不包括根节点，只有根节点才是1，如果有两个节点[1,2]
+     * 那么2 作为叶子节点，跟跟节点组成 路径
+     * 所以如果left null,最小等于 minDepth(root.right) + 1;
+     * @param root
+     * @return
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null) return minDepth(root.right) + 1;
+        if (root.right == null) return minDepth(root.left) +1;
+        return 1 + Math.min(minDepth(root.left),minDepth(root.right));
+
+    }
+
+}
