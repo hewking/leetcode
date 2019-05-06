1b583e9937ff05d726b9aa49a8d4d91d6f642ef9
hewking <604910070qq.com>
2019-05-07 00:53:22 +0800

    update 修剪二叉树

--- leetcode2/src/leetcode/TrimABinarySearchTree.java
+++ leetcode2/src/leetcode/TrimABinarySearchTree.java
@@ -52,7 +52,16 @@ public class TrimABinarySearchTree {
      * 节点比左子节点大，比右子节点小
      */
     public TreeNode trimBST(TreeNode root, int L, int R) {
-        return null;
+        if (root == null) return null;
+        if (root.val > R) {
+            return trimBST(root.left,L,R);
+        } else if (root.val < L) {
+            return trimBST(root.right,L,R);
+        } else {
+            root.left = trimBST(root.left,L,R);
+            root.right = trimBST(root.right,L,R);
+        }
+        return root;
     }
 
 }
