9c54735f1be8025fff47aac9702304266e78bcdf
hewking <604910070@qq.com>
2019-05-06 10:58:33 +0800

    add 左叶子节点之和 单值树判断，修剪二叉搜索树

---
+++ leetcode2/src/leetcode/SumOfLeftLeaves.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+/**
+ *左子叶之和
+ * https://leetcode-cn.com/problems/sum-of-left-leaves/
+ * 计算给定二叉树的所有左叶子之和。
+ *
+ * 示例：
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ * @program: leetcode
+ * @description: ${description}
+ * @author: hewking
+ * @create: 2019-05-06 10:20
+ **/
+public class SumOfLeftLeaves {
+
+    /**
+     * 思路：
+     * 1.看起来很简单，只需要计算左子叶
+     * 2.基准情形：
+     *3.叶子节点注意，这意味着不包括左子叶几点的父节点
+     */
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        // 左子叶节点条件
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+
+}
