d48afdaebb47bc644c79633a41a2072f857bbd05
hewking <604910070@qq.com>
2019-05-08 11:37:15 +0800

    add 二叉树路径 二叉树直径

---
+++ leetcode2/src/leetcode/BinaryTreePaths.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二叉树的所有路径
+ * https://leetcode-cn.com/problems/binary-tree-paths/
+ * 给定一个二叉树，返回所有从根节点到叶子节点的路径。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ *
+ * 输入:
+ *
+ *    1
+ *  /   \
+ * 2     3
+ *  \
+ *   5
+ *
+ * 输出: ["1->2->5", "1->3"]
+ *
+ * 解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
+ * @program: leetcode
+ * @description: ${description}
+ * @author: hewking
+ * @create: 2019-05-08 09:42
+ **/
+public class BinaryTreePaths {
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        if (root == null) return paths;
+        path(root,paths,"");
+        return paths;
+    }
+
+    public void path(TreeNode root,List<String> paths,String path) {
+        if (isLeaf(root)) {
+            path += root.val;
+            paths.add(path);
+        } else {
+            if (root != null) {
+                path += root.val + "->";
+                path(root.left,paths,path);
+                path(root.right,paths,path);
+            }
+        }
+    }
+
+    public boolean isLeaf(TreeNode root) {
+        return root != null && root.left == null && root.right == null;
+    }
+}
---
+++ leetcode2/src/leetcode/DiameterOfBinaryTree.java
@@ -0,0 +1,59 @@
+package leetcode;
+
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * 二叉树的直径
+ * https://leetcode-cn.com/problems/diameter-of-binary-tree/
+ * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过根结点。
+ *
+ * 示例 :
+ * 给定二叉树
+ *
+ *           1
+ *          / \
+ *         2   3
+ *        / \
+ *       4   5
+ * 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+ *
+ * 注意：两结点之间的路径长度是以它们之间边的数目表示。
+ * @program: leetcode
+ * @description: ${description}
+ * @author: hewking
+ * @create: 2019-05-08 11:04
+ **/
+public class DiameterOfBinaryTree {
+
+    /**
+     * 思路
+     * 1.直径定义：任意两个节点路径长度的最大值，
+     * 2.所以需要计算节点的路径长度
+     * 3. 还有那个节点的路径长度比根节点出发的路径更长吗？错了，理解有误，
+     * 是两个节点之间，而不是根节点到子节点，妈的 这感觉是个图的问题啊，虽说
+     * 树是图的一个特列
+     *
+     * 唉，想多了
+     * 因为我想到，子节点加上父节点的长度了，这再递归中不行的
+     * 这样是父节点加子节点的
+     */
+
+    int max;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        max = Math.max(max ,leftHeight + rightHeight + 1);
+        // 子节点进行判断最大长度
+        diameterOfBinaryTree(root.left);
+        diameterOfBinaryTree(root.right);
+        return max -1;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) return 0;
+        return  1 + Math.max(height(root.left),height(root.right));
+    }
+
+}
