2ef40e239ba4b6bd4034b1069a358ba7d81409cf
hewking <cjh1095358798@gmail.com>
2019-07-17 01:23:44 +0800

    add 448 找到数组中消失的数字

---
+++ leetcode2/src/leetcode/find-all-numbers-disappeared-in-an-array.kt
@@ -0,0 +1,48 @@
+package leetcode
+
+import java.util.*
+
+/**448. 找到所有数组中消失的数字
+ * https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/
+ * Created by test
+ * Date 2019/7/17 0:56
+ * Description
+ *给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+
+找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+
+您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+
+示例:
+
+输入:
+[4,3,2,7,8,2,3,1]
+
+输出:
+[5,6]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+object FindAllNumbersDisappearedInAnArray {
+
+    @JvmStatic
+    fun main(args:Array<String>) {
+
+    }
+
+    class Solution {
+        fun findDisappearedNumbers(nums: IntArray): List<Int> {
+            val result = mutableListOf<Int>()
+            Arrays.sort(nums)
+            for (i in 1 .. nums.size) {
+                if (Arrays.binarySearch(nums,i) < 0) {
+                    result.add(i)
+                }
+            }
+            return result
+        }
+    }
+}
