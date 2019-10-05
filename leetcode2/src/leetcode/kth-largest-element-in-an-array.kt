c3832d074f0250609f4dec166917e888c57a55ea
hewking <cjh1095358798@gmail.com>
2019-10-06 00:30:35 +0800

    add 数组中最大的第k个数

---
+++ leetcode2/src/leetcode/kth-largest-element-in-an-array.kt
@@ -0,0 +1,57 @@
+package leetcode
+
+import java.util.*
+import kotlin.Comparator
+
+/**
+ * 215. 数组中的第K个最大元素
+ * https://leetcode-cn.com/problems/kth-largest-element-in-an-array/
+ * Created by test
+ * Date 2019/10/6 0:11
+ * Description
+ * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+示例 1:
+
+输入: [3,2,1,5,6,4] 和 k = 2
+输出: 5
+示例 2:
+
+输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+输出: 4
+说明:
+
+你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/kth-largest-element-in-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+object KthlargestElementInAnArray {
+
+    class Solution {
+
+        var i = 0
+        var res = 0
+
+        /**
+         * 思路：
+         * 排序
+         */
+        fun findKthLargest(nums: IntArray, k: Int): Int {
+            nums.sortDescending()
+
+            findK(nums,k)
+            return this.res
+        }
+
+        fun findK(nums: IntArray,k: Int) {
+            if (++i == k) {
+                res = nums[i]
+                return
+            }
+            findK(nums,k)
+        }
+    }
+
+}
