8f93c9166cba5ffd9572b95f4b2034de6d2d22dc
hewking <cjh1095358798@gmail.com>
2019-10-31 14:38:45 +0800

    add 括号生产

---
+++ leetcode2/src/leetcode/generate-parentheses.kt
@@ -0,0 +1,37 @@
+package leetcode
+
+/**
+ * 22. 括号生成
+ * https://leetcode-cn.com/problems/generate-parentheses/
+ *
+ * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+
+例如，给出 n = 3，生成结果为：
+
+[
+"((()))",
+"(()())",
+"(())()",
+"()(())",
+"()()()"
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/generate-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        **/
+
+object GenerateParentheses {
+
+    class Solution {
+        fun generateParenthesis(n: Int): List<String> {
+            val ans = mutableListOf<String>()
+            return ans
+        }
+    }
+
+}
+
+fun main() {
+
+}
