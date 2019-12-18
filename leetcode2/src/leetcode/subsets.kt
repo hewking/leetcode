f159853c262c68f8ab6c1c7047577c252824169e
hewking <cjh1095358798@gmail.com>
2019-12-18 12:00:54 +0800

    add 子集

--- leetcode2/src/leetcode/subsets.kt
+++ leetcode2/src/leetcode/subsets.kt
@@ -1,5 +1,9 @@
 package leetcode
 
+import java.util.ArrayList
+
+
+
 /**
  * 78. 子集
  *https://leetcode-cn.com/problems/subsets/
@@ -31,9 +35,51 @@ package leetcode
 object Subsets {
 
     class Solution {
-        fun subsets(nums: IntArray): List<List<Int>> {
 
+        val ans = mutableListOf<List<Int>>()
+
+
+        fun subsets(nums: IntArray): List<List<Int>> {
+            nums.sort()
+            subset2(nums, mutableListOf(),0)
+            return ans
         }
+
+        /**
+         * 思路：
+         * 有点问题，没考虑周全
+         */
+        fun subset(nums:IntArray,ans: MutableList<List<Int>>,index: Int) {
+            if (index == nums.size) {
+                ans.add(mutableListOf())
+                return
+            }
+            ans.add(mutableListOf(nums[index]))
+            for (i in index + 1 until nums.size) {
+                ans.add(mutableListOf(nums[index],nums[i]))
+            }
+            if (nums.size - index > 2) {
+                val lastElement = nums.toMutableList().subList(index,nums.size)
+                ans.add(lastElement)
+            }
+            subset(nums,ans,index + 1)
+        }
+
+        /**
+         * 思路2：
+         * 跟全排列有点像
+         * https://leetcode-cn.com/problems/subsets/solution/xiang-xi-jie-shao-di-gui-hui-su-de-tao-lu-by-reedf/
+         */
+        fun subset2(nums:IntArray,list: MutableList<Int>,index: Int) {
+            ans.add(list.toList())
+            for (i in index until nums.size) {
+                list.add(nums[i])
+                subset2(nums,list,i + 1)
+                list.removeAt(list.size - 1) // 递归完每次移除添加的
+            }
+        }
+
+
     }
 
 }
